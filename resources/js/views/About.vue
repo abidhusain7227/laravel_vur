d633f3c772585cb2d707e6471a079f76cec33af2
Abidhusain Bhoraniya <abidhusain.alteza@gmail.com>
2023-01-12 18:06:29 +0530

    loop and leedcode

--- resources/js/views/About.vue
+++ resources/js/views/About.vue
@@ -37,13 +37,53 @@ export default {
                 }
             }
         },
+
+        loop(){
+          let arry = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100];
+          let slice;
+          let data = [];
+          for (let index = 0; index < arry.length;) {
+            const element = arry[index];
+            slice = arry.slice(index,4+index);
+            data.push(slice);
+            index = index + 4;
+            
+          }
+          console.log('abid',data);
+          console.log(arry.length)
+        },
+
+        addTwoNumbers(){
+          let l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+          let rel1 = l1.reverse().toString().split(',').join("");
+          let rel2 = l2.reverse().toString().split(',').join("");
+          let sum = Number(rel1) + Number(rel2)
+          let myFunc = num => Number(num);
+          var output = Array.from(String(sum), myFunc);
+          output = output.reverse()
+          return output;
+        },
+         isPalindrome(x=10) {
+            const isNegative = x< 0 ? true : false;
+            if (isNegative){
+                return false;
+            }
+            const temp = x;
+            let reversed = 0;
+              
+            while(x>0){
+                reversed = (reversed * 10) + (x%10);
+                x = parseInt(x/10);
+            }
+            return reversed == temp;
+          }
     },
 };
 </script>
 
 <template>
     <div>
-        <button @click="TwoSum()">TwoSum</button>
-        <h1>{{ TwoSum() }}</h1>
+        <button @click="isPalindrome()">isPalindrome</button>
+        <h1>{{ isPalindrome() }}</h1>
     </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,13 @@ use Illuminate\Support\Facades\Route;
 
 
 
-// Route::get('/{any}', function () {
-//     return view('layouts.app');
-// })->where('any', '.*');
+Route::get('/{any}', function () {
+    return view('layouts.app');
+})->where('any', '.*');
 
 // Route::get('test',[amezonController::class,'Amazon']);
 // Route::get('carbon',[amezonController::class,'Carbon']);
 
-Route::get('/loop', function () {
-    return view('loop');
-});
+// Route::get('/loop', function () {
+//     return view('loop');
+// });
